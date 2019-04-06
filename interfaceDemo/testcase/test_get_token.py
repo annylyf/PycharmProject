6f8b3bbb926e35f2ac0ce525ec9eeb6659dbcb0d
annylyf <1063528575@qq.com>
2019-04-06 13:28:32 +0800

    配置代理，使用代理

--- interfaceDemo/testcase/test_get_token.py
+++ interfaceDemo/testcase/test_get_token.py
@@ -1,8 +1,16 @@
 import requests
 
 def test_token():
-    url="https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=wwd6fc5a5c802ec799&corpsecret=N6nw0IQVwBfdLASfXem2OohB6WyMahaaGCaMYbrTaU8"
-    res=requests.get(url)
+    corpid="wwd6fc5a5c802ec799"
+    corpsecret="N6nw0IQVwBfdLASfXem2OohB6WyMahaaGCaMYbrTaU8"
+    url="https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid="+corpid+"&corpsecret="+corpsecret
+   #设置代理
+    myproxies = {
+        'http':'192.168.0.106:8889',
+        'https': '192.168.0.106:8889',
+    }
+    #使用代理，可以在本地代理上查看到接口的请求数据
+    res=requests.get(url,proxies=myproxies,verify=False)
     print(res.json())
     qiye_token=res.json().get("access_token")
     return qiye_token
--- interfaceDemo/testcase/test_upload_image.py
+++ interfaceDemo/testcase/test_upload_image.py
@@ -1,10 +1,19 @@
 
 import requests
+from testcase import test_get_token
+import logging
 
 def test_upload_image():
-    ACCESS_TOKE = "7Gwqo0iuit2c1uMkfgLwEIoImfK6pDTZI18wFqt-1bzo7NeFiibPfQa5UbpDnt6FYQzf5CbhUV5Ea8be3UtruS_wBk-Igp6Wihj95a0ZVl9jFMbboWLG0FpVaQqE1At3PvBnDGtEG6vgPbC7waoK_r9_fOALFLkjoT5gZQMSL0zYfFM7A8y5gmid6vw_3huoOnt4Zzw9nux9T1pUH5V4vg"
+    # ACCESS_TOKE = "7Gwqo0iuit2c1uMkfgLwEIoImfK6pDTZI18wFqt-1bzo7NeFiibPfQa5UbpDnt6FYQzf5CbhUV5Ea8be3UtruS_wBk-Igp6Wihj95a0ZVl9jFMbboWLG0FpVaQqE1At3PvBnDGtEG6vgPbC7waoK_r9_fOALFLkjoT5gZQMSL0zYfFM7A8y5gmid6vw_3huoOnt4Zzw9nux9T1pUH5V4vg"
+    #使用动态的token
+    ACCESS_TOKE=test_get_token.test_token()
     url="https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token="+ACCESS_TOKE
-    upload_file = {'media':('1111.jpg',open('1111.jpg','rb'),'image/jpg')}
-    res=requests.post(url,files=upload_file)
+    upload_file = {'media':('you.jpg',open('you.jpg','rb'),'image/jpg')}
+    #发送post请求，将请求结果赋值到res
+    res=requests.post(url,files=upload_file,proxies=)
+    #断言，测试返回结果是否符合预期
     assert res.json().get('errcode')==0
+    logging.debug("debug message")
+    media_id=res.json().get('media_id')
+    print(media_id)
     print(res.json())
